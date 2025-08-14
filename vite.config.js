7891a4e1cbc44793189a20794cf43bc9b143f74a
Stef van den Oever <stef@vandenoever.me>
2025-08-14 14:42:18 +0200

    fix: use .js entry to avoid JSX MIME error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ include: '**/*.{jsx,js}' })],
   build: {
     rollupOptions: {
-      input: 'src/main.jsx',
+      input: 'src/main.js',
       output: {
         entryFileNames: 'main.js',
         format: 'es'
