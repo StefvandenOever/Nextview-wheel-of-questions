c81d27458d5ebc724a49017c30a76c466ae205e7
Stef van den Oever <stef@vandenoever.me>
2025-08-14 14:51:56 +0200

    fix: compile React app

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react({ include: '**/*.{jsx,js}' })],
   build: {
-    rollupOptions: {
-      input: 'src/main.js',
-      output: {
-        entryFileNames: 'main.js',
-        format: 'es'
-      }
-    },
+      rollupOptions: {
+        input: 'src/main.jsx',
+        output: {
+          entryFileNames: 'main.js',
+          format: 'es'
+        }
+      },
     outDir: 'dist',
     emptyOutDir: true
   }
